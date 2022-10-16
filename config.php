cd033048d6b2f03a5e859ecbb3409d9dab443bf6
sscefalix <100369942+sscefalix@users.noreply.github.com>
2022-10-16 21:37:41 +0300

    Create config.php

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+define("CLIENT_SECRET", "client_secret");
+define("CLIENT_ID", "client_id");
+define("LOGIN_URL", "http://localhost/login.php");
+define("REDIRECT_URI", "client_redirect_uri");
+?>
