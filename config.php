b55670ef5124b85d493c05c839bddfc206032679
sscefalix <100369942+sscefalix@users.noreply.github.com>
2022-10-16 22:10:10 +0300

    Delete config.php

--- config.php
+++
@@ -1,6 +0,0 @@
-<?php
-define("CLIENT_SECRET", "client_secret");
-define("CLIENT_ID", "client_id");
-define("LOGIN_URL", "http://localhost/login.php");
-define("REDIRECT_URI", "client_redirect_uri");
-?>
